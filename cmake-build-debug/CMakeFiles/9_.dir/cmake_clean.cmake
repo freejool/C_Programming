dd33306723160d166a0a0f093ef42ebbb25a73ba
Sxing <sxy1066740232@gmail.com>
2021-05-19 10:17:15 +0800

    0.0

---
+++ cmake-build-debug/CMakeFiles/9_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "9_"
+  "9_.pdb"
+  "CMakeFiles/9_.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/9_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/9_.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 9_.
+# This may be replaced when dependencies are built.
